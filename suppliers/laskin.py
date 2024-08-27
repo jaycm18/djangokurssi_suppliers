b97f419855497604e20b1a11ee20e1fe198af3f1
Joonas Montonen <159522623+jaycm18@users.noreply.github.com>
2024-08-27 16:04:52 +0300

    testit

---
+++ suppliers/laskin.py
@@ -0,0 +1,10 @@
+# laskinfunktio yksikkötestien demoamiseen
+
+def plus(x, y):
+    return x + y
+
+def plus_complicated(x, y):
+    if x > y:
+        return x + y
+    else:
+        return y
---
+++ suppliers/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+import unittest
+
+from suppliers.laskin import plus , plus_complicated
+
+class LaskinTests(TestCase):
+    def test_plus(self):
+        # testaa että numerot lasketaan yhteen
+        self.assertEqual(plus(7, 2), 9)
+        self.assertEqual(plus(7.1, 2.7), 9.8)
+
+    def test_plus_complicated(self):
+        # testing adding with conditional if clause
+        self.assertEqual(plus_complicated(7, 2), 9)
+        self.assertEqual(plus_complicated(2, 8), 8)
+    
+    @unittest.expectedFailure
+    def test_plus_should_fail(self):
+        self.assertEqual(plus('7', 2), 9)
+
+    # TDD - Test Driven Development
+    
