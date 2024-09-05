8738bcce9c08e4dadcf5fddf9351352850de19db
Joonas Montonen <159522623+jaycm18@users.noreply.github.com>
2024-09-05 13:12:05 +0300

    loginit

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import deletesupplier, landingview, productlistview, supplierlistview, deletesupplier, products_filtered
+from .views import deletesupplier, productlistview, supplierlistview, deletesupplier, products_filtered
 from .views import addsupplier, addproduct, deleteproduct, confirmdeleteproduct, confirmdeletesupplier
-from .views import edit_product_post, edit_product_get, searchsuppliers
+from .views import edit_product_post, edit_product_get, searchsuppliers, loginview, login_action, logout_action
 
 urlpatterns = [
-    path('', landingview),
+    # Landing page after login
+    #path('landing', landingview),
+
+    # Loginview and authentication methods
+    path('', loginview),
+    path('login/', login_action),
+    path('logout/', logout_action),
 
     # Products url´s
     path('products/', productlistview),
--- app/views.py
+++ app/views.py
@@ -1,26 +1,62 @@
 from django.shortcuts import render, redirect
 from .models import Supplier, Product
+from django.contrib.auth import authenticate, login, logout
+
+'''# Landing afrer login
 
 def landingview(request):
     return render(request, 'landingpage.html')
+'''
+
+# Login and logout
+
+def loginview(request):
+    return render(request, 'loginpage.html')
+
+def login_action(request):
+    user = request.POST['username']
+    passw = request.POST['password']
+    # Löytyykö kyseistä käyttäjää?
+    user = authenticate(username = user, password = passw)
+    #Jos löytyy:
+    if user:
+        # Kirjataan sisään
+        login(request, user)
+        # Tervehdystä varten context
+        context = {'name': user}
+        # Kutsutaan suoraan landingview.html
+        return render(request,'landingpage.html',context)
+    # Jos ei kyseistä käyttäjää löydy
+    else:
+        return render(request, 'loginerror.html')
+    
+def logout_action(request):
+    logout(request)
+    return render(request, 'loginpage.html')
 
 # Product view´s
 
 def productlistview(request):
-    productlist = Product.objects.all()
-    supplierlist = Supplier.objects.all()
-    context = {'products': productlist, 'suppliers': supplierlist}
-    return render (request,"productlist.html",context)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        productlist = Product.objects.all()
+        supplierlist = Supplier.objects.all()
+        context = {'products': productlist, 'suppliers': supplierlist}
+        return render (request,"productlist.html",context)
 
 def addproduct(request):
-    a = request.POST['productname']
-    b = request.POST['packagesize']
-    c = request.POST['unitprice']
-    d = request.POST['unitsinstock']
-    e = request.POST['supplier']
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        a = request.POST['productname']
+        b = request.POST['packagesize']
+        c = request.POST['unitprice']
+        d = request.POST['unitsinstock']
+        e = request.POST['supplier']
     
-    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
-    return redirect(request.META['HTTP_REFERER'])
+        Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
+        return redirect(request.META['HTTP_REFERER'])
 
 def confirmdeleteproduct(request, id):
     product = Product.objects.get(id = id)
@@ -53,19 +89,25 @@ def products_filtered(request, id):
 # Supplier view´s
 
 def supplierlistview(request):
-    supplierlist = Supplier.objects.all()
-    context = {'suppliers': supplierlist}
-    return render (request,"supplierlist.html",context)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        supplierlist = Supplier.objects.all()
+        context = {'suppliers': supplierlist}
+        return render (request,"supplierlist.html",context)
 
 def addsupplier(request):
-    a = request.POST['companyname']
-    b = request.POST['contactname']
-    c = request.POST['address']
-    d = request.POST['phone']
-    e = request.POST['email']
-    f = request.POST['country']
-    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
-    return redirect(request.META['HTTP_REFERER'])
+        if not request.user.is_authenticated:
+            return render(request, 'loginpage.html')
+        else:
+            a = request.POST['companyname']
+            b = request.POST['contactname']
+            c = request.POST['address']
+            d = request.POST['phone']
+            e = request.POST['email']
+            f = request.POST['country']
+            Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+            return redirect(request.META['HTTP_REFERER'])
 
 def confirmdeletesupplier(request, id):
     supplier = Supplier.objects.get(id = id)
