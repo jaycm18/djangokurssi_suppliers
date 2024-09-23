aed4723df8883666d5f74b6353e97bbe4e6c689a
Joonas Montonen <159522623+jaycm18@users.noreply.github.com>
2024-09-23 10:26:00 +0300

    debug false

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fqz^un)a&(k3jyh#yigzz7&a0fzcw(%7a6qeto9rbn4uiau706'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
